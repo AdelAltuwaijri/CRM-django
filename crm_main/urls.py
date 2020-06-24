940fb53259b5f12adddb793889ed28c3371a8a2c
AdelAltuwaijri <eng.aaltuwaijri@gmail.com>
2020-06-24 07:57:31 +0300

    add user authentication templates-urls

--- crm_main/urls.py
+++ crm_main/urls.py
@@ -29,4 +29,30 @@ urlpatterns = [
             auth_views.PasswordChangeView.as_view(
                 template_name="common/changepassword.html",success_url=reverse_lazy('home')),
                 name="changepassword"),
+
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='common/password-reset/password_reset.html',
+             subject_template_name='common/password-reset/password_reset_subject.txt',
+             email_template_name='common/password-reset/password_reset_email.html',
+              success_url='/login/'
+         ),
+         name='password_reset'),
+
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='common/password-reset/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='common/password-reset/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='common/password-reset/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
